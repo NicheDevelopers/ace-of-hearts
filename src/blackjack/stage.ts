3a04056483a71e8d561ac7350ef406a16b078373
oskkil <oskkil@onet.pl>
2024-09-28 12:56:10 +0200

    Add blackjack engine

--- src/blackjack/stage.ts
+++ src/blackjack/stage.ts
@@ -1,5 +1,13 @@
 import * as PIXI from 'pixi.js';
+const blackjack = require('blackjack-core');
 
 const blackjackStage = new PIXI.Container();
+const Game = blackjack.Game;
+const actions = blackjack.actions;
+
+const game = new Game();
+
+console.log(game.getState());
+game.dispatch(actions.deal());
 
 export default blackjackStage;
